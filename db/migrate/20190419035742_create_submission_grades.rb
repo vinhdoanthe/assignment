3f423ea2c7d93c2e5db270935f9f6f027508b999
Vinh Doan-The <vinhdt@funix.edu.vn>
2019-05-06 16:14:54 +0700

    Modify CreateSubmissionGrades migration

--- db/migrate/20190419035742_create_submission_grades.rb
+++ db/migrate/20190419035742_create_submission_grades.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class CreateSubmissionGrades < ActiveRecord::Migration[5.2]
   def change
     create_table :submission_grades do |t|
-      t.belongs_to :assignment, foreign_key: true
-      t.references :student, foreign_key: true
+      t.belongs_to :assignment, class_name: 'User', foreign_key: 'student_id'
+      t.references :student, class_name => 'User', :foreign_key => 'mentor_id', :optional => true
       t.string :submission_status
-      t.references :submission_file, foreign_key: true
+      # t.references :submission_file, foreign_key: true
       t.integer :attempt_count
       t.boolean :latest
       t.references :mentor, foreign_key: true
       t.string :grade_status
       t.belongs_to :graded_rubric, foreign_key: true
-      t.references :graded_file, foreign_key: true
+      # t.references :graded_file, foreign_key: true
       t.decimal :point
 
       t.timestamps
