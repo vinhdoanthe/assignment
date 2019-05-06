a3d84e9eed046df5607472a92be602c7847f2748
Vinh Doan-The <vinhdt@funix.edu.vn>
2019-05-06 16:29:17 +0700

    Update migration files

--- db/migrate/20190419051108_drop_columns_submission_grades_table.rb
+++
@@ -1,6 +0,0 @@
-class DropColumnsSubmissionGradesTable < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :submission_grades, :submission_file_id
-    remove_column :submission_grades, :graded_file_id 
-  end
-end
