3bee6279807371bdc4e4d875f5a9aba567bc61e7
Vinh Doan-The <vinhdt@funix.edu.vn>
2019-05-15 18:05:11 +0700

    Update migrations

--- db/migrate/20190509092028_change_graded_type.rb
+++ db/migrate/20190509092028_change_graded_type.rb
@@ -1,6 +1,5 @@
 class ChangeGradedType < ActiveRecord::Migration[5.2]
   def change
-    rename_column :graded_criteria, :status, :status
     change_column :graded_criteria, :status, :string
   end
 end
