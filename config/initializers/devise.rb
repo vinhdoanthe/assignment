de04ab53111448e668cb7563b396423074e284ab
Vinh Doan-The <vinhdt@funix.edu.vn>
2019-05-24 16:35:26 +0700

    Store OAuth2 private information to private settings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,7 +259,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :google_oauth2, '683505809827-1iblia1ma8ef0n7iebhcmsrcqalaeau7.apps.googleusercontent.com', '5XtPOiosbzgWbbod3KpFqF4L', { }
+  config.omniauth :google_oauth2, ENV["GOOGLE_OAUTH_CLIENT_ID"], ENV["GOOGLE_OAUTH_SECRET"], { }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
