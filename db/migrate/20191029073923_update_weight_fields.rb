bdec97cb51c275f1d602a2b230478a7df684db03
Vinh Doan-The <vinhdt@funix.edu.vn>
2019-10-29 14:46:18 +0700

    Update weight fields

--- db/migrate/20191029073923_update_weight_fields.rb
+++ db/migrate/20191029073923_update_weight_fields.rb
@@ -2,10 +2,12 @@ class UpdateWeightFields < ActiveRecord::Migration[5.2]
   def change
     def up
       change_column :criteria_formats, :weight, :decimal, precision: 10, scale: 2, default: 0.0
+      change_column :graded_criteria, :weight, :decimal, precision: 10, scale: 2, default: 0.0
     end
 
     def down
       change_column :criteria_formats, :weight, :decimal, default: 0.0
+      change_column :graded_criteria, :weight, :decimal, default: 0.0
     end
   end
 end
