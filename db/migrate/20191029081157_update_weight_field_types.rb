75a0cd9032e12458fdd1a5c094ae2037d49b9924
Vinh Doan-The <vinhdt@funix.edu.vn>
2019-10-29 15:14:56 +0700

    Update field types

---
+++ db/migrate/20191029081157_update_weight_field_types.rb
@@ -0,0 +1,11 @@
+class UpdateWeightFieldTypes < ActiveRecord::Migration[5.2]
+  def up
+    change_column :criteria_formats, :weight, :decimal, :precision => 4, :scale => 2, :default => 00.00
+    change_column :graded_criteria, :weight, :decimal, :precision => 4, :scale => 2, :default => 00.00
+  end
+
+  def down
+    change_column :criteria_formats, :weight, :decimal
+    change_column :graded_criteria, :weight, :decimal
+  end
+end
