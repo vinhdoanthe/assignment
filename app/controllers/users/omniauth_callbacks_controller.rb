e7f3b0e3bb3ab6003682bc0f177e796bc57e138f
The-Vinh Doan <doanthevinh1991@gmail.com>
2020-03-22 20:24:49 +0700

    Update Google Oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,6 +13,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
     end
   end
+
   def azureactivedirectory
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.nil?
